75f3f5e85e13e6efc2f8dbfc04a0b90de67ca3f2
Denton Kerr <dentonkerr@gmail.com>
2018-03-24 11:04:19 -0400

    adds img url to somebooks seeder

--- seeders/test/20180321034519-somebooks-library.js
+++ seeders/test/20180321034519-somebooks-library.js
@@ -6,6 +6,7 @@ module.exports = {
         title: "Weaveworld",
         author: "Clive Barker",
         genre: "Fantasy/Horror",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.whocares.com",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -14,6 +15,7 @@ module.exports = {
         title: "The Necronomicon",
         author: "Abdul Alhazred",
         genre: "Self-Help",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.hell.org",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -22,6 +24,7 @@ module.exports = {
         title: "The Holy Bible",
         author: "Man",
         genre: "Fiction",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.eternalsalvationoryourmoneyback.com",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -30,6 +33,7 @@ module.exports = {
         title: "Gaia: A New Look At Life On Earth",
         author: "J.E. Lovelock",
         genre: "Earth Science",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.alivingworld.org",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -38,6 +42,7 @@ module.exports = {
         title: "The New York City Cab Driver's Joke Book",
         author: "Jim Pietsch",
         genre: "Humor",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.hahaha.com",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -46,6 +51,7 @@ module.exports = {
         title: "World War Z",
         author: "Max Brooks",
         genre: "Fiction",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.betterthanthemovie.com",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -54,6 +60,7 @@ module.exports = {
         title: "A Brief History of Time",
         author: "Stephen Hawking",
         genre: "Science",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.gangsta.com",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -62,6 +69,7 @@ module.exports = {
         title: "The Long, Dark Tea Time of the Soul",
         author: "Douglas Adams",
         genre: "Humor/Fiction",
+        img: 'https://www.pinterest.com/pin/572942383817660709/',
         url: "https://www.holisticdriving.com",
         createdAt: new Date(),
         updatedAt: new Date()
